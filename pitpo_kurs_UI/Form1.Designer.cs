4774a64512fe72de64291ac93c8a10059021a816
cr1stal <yaroslav3333.ff@gmail.com>
2025-07-16 15:31:53 +0300

    1 commit

---
+++ pitpo_kurs_UI/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace pitpo_kurs_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSectionsEnemies = new System.Windows.Forms.Button();
+            this.btnSectionsArtifacts = new System.Windows.Forms.Button();
+            this.btnSectionsCharacters = new System.Windows.Forms.Button();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtMinViews = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(219, 309);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(344, 215);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(247, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(527, 82);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Добро пожаловать в энциклопедию \r\nпо игре Shadows of the Soul";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(715, 55);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(191, 29);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Поиск";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(36, 55);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(673, 27);
+            this.txtSearch.TabIndex = 10;
+            // 
+            // btnSectionsEnemies
+            // 
+            this.btnSectionsEnemies.Location = new System.Drawing.Point(36, 357);
+            this.btnSectionsEnemies.Name = "btnSectionsEnemies";
+            this.btnSectionsEnemies.Size = new System.Drawing.Size(116, 79);
+            this.btnSectionsEnemies.TabIndex = 9;
+            this.btnSectionsEnemies.Text = "Враги";
+            this.btnSectionsEnemies.UseVisualStyleBackColor = true;
+            // 
+            // btnSectionsArtifacts
+            // 
+            this.btnSectionsArtifacts.Location = new System.Drawing.Point(36, 277);
+            this.btnSectionsArtifacts.Name = "btnSectionsArtifacts";
+            this.btnSectionsArtifacts.Size = new System.Drawing.Size(116, 74);
+            this.btnSectionsArtifacts.TabIndex = 8;
+            this.btnSectionsArtifacts.Text = "Артефакты";
+            this.btnSectionsArtifacts.UseVisualStyleBackColor = true;
+            // 
+            // btnSectionsCharacters
+            // 
+            this.btnSectionsCharacters.Location = new System.Drawing.Point(36, 197);
+            this.btnSectionsCharacters.Name = "btnSectionsCharacters";
+            this.btnSectionsCharacters.Size = new System.Drawing.Size(116, 74);
+            this.btnSectionsCharacters.TabIndex = 7;
+            this.btnSectionsCharacters.Text = "Персонажи";
+            this.btnSectionsCharacters.UseVisualStyleBackColor = true;
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.ItemHeight = 20;
+            this.lstResults.Location = new System.Drawing.Point(219, 197);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(344, 104);
+            this.lstResults.TabIndex = 14;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(569, 206);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(307, 272);
+            this.txtDescription.TabIndex = 15;
+            // 
+            // txtMinViews
+            // 
+            this.txtMinViews.Location = new System.Drawing.Point(36, 102);
+            this.txtMinViews.Name = "txtMinViews";
+            this.txtMinViews.Size = new System.Drawing.Size(125, 27);
+            this.txtMinViews.TabIndex = 16;
+            this.txtMinViews.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Количество просмотров";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 536);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMinViews);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSectionsEnemies);
+            this.Controls.Add(this.btnSectionsArtifacts);
+            this.Controls.Add(this.btnSectionsCharacters);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSectionsEnemies;
+        private System.Windows.Forms.Button btnSectionsArtifacts;
+        private System.Windows.Forms.Button btnSectionsCharacters;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtMinViews;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ pitpo_kurs_UI/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace pitpo_kurs_UI
+{
+    public partial class Form1 : Form
+    {
+        private string _section = "Персонажи";
+        private const string ConnStr = "Host=localhost;Port=5432;Username=postgres;Password=12345;Database=postgres";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstResults.DisplayMember = "Title";
+            lstResults.SelectedIndexChanged += (s, ev) => ShowDetails();
+
+            btnSectionsCharacters.Click += (s, ev) => SwitchSection("Персонажи");
+            btnSectionsArtifacts.Click += (s, ev) => SwitchSection("Артефакты");
+            btnSectionsEnemies.Click += (s, ev) => SwitchSection("Враги");
+
+            btnSearch.Click += (s, ev) => RefreshList();
+
+            SwitchSection(_section);
+        }
+        private void SwitchSection(string section)
+        {
+            _section = section;
+            txtSearch.Clear();
+            txtMinViews.Text = "0";
+            lstResults.Items.Clear();
+            txtDescription.Clear();
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            lstResults.Items.Clear();
+            if (!int.TryParse(txtMinViews.Text, out int minViews))
+                minViews = 0;
+
+            string keyword = txtSearch.Text;
+            List<Article> filteredArticles = GetFilteredArticles(minViews, keyword);
+            filteredArticles.ForEach(a => lstResults.Items.Add(a));
+        }
+
+
+        public List<Article> GetFilteredArticles(int minViews, string keyword)
+        {
+            List<Article> articles = GetArticles();
+            if (articles == null)
+            {
+                return new List<Article>();//ioio
+            }
+
+            string lowerKeyword = string.IsNullOrWhiteSpace(keyword) ? "" : keyword.Trim().ToLower();
+
+            List<Article> filteredArticles = new List<Article>();
+
+            foreach (var article in articles)
+            {
+                if (article.Views < minViews)
+                    continue;
+
+                if (string.IsNullOrEmpty(lowerKeyword))
+                {
+                    if (article.Tags != null &&
+                        article.Tags.Any(tag => tag.Equals(_section, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        filteredArticles.Add(article);
+                    }
+                }
+                else
+                {
+                    bool matchesTitle = article.Title.ToLower().Contains(lowerKeyword);
+                    bool matchesTags = article.Tags != null &&
+                                       article.Tags.Any(tag => tag.ToLower().Contains(lowerKeyword));
+
+                    if (matchesTitle || matchesTags)
+                        filteredArticles.Add(article);
+                }
+            }
+
+            return filteredArticles;
+        }
+
+
+
+        private void ShowDetails()
+        {
+            if (lstResults.SelectedItem is Article art)
+            {
+                txtDescription.Text =
+                    $"Название: {art.Title}\r\n" +
+                    $"Описание: {art.Description}\r\n" +
+                    $"Просмотров: {art.Views}\r\n" +
+                    $"Теги: {string.Join(", ", art.Tags)}";
+            }
+        }
+
+        private List<Article> GetArticles()
+        {
+            var list = new List<Article>();
+            using var conn = new NpgsqlConnection(ConnStr);
+            conn.Open();
+            using var cmd = new NpgsqlCommand(
+                "SELECT id, title, description, views, tags FROM articles", conn);
+            using var rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                list.Add(new Article
+                {
+                    Id = rd.GetInt32(0),
+                    Title = rd.GetString(1),
+                    Description = rd.GetString(2),
+                    Views = rd.GetInt32(3),
+                    Tags = !rd.IsDBNull(4)
+                        ? rd.GetString(4)
+                              .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                              .ToList()
+                        : new List<string>()
+                });
+            }
+            return list;
+        }
+
+    }
+
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Views { get; set; }
+        public List<string> Tags { get; set; }
+        public override string ToString() => Title;
+    }
+}
+    
+
